1551d92d97df43222c2197131755c1caa93f4b94
JaeTriesGit <boffering@hotmail.com>
2023-05-29 11:54:06 +0300

    oh no react

---
+++ components/item.jsx
@@ -0,0 +1,17 @@
+import Trashcan from '../assets/trashcan.svg'
+
+export default function Item(){
+
+    return(
+        <div class="Item">
+            <div class="Img">
+                <img class="Item-Image" src=""/>
+            </div>
+            <div class="Item-Info">
+                <p class="Name">Test Item <span>249,98€</span></p>
+                <p class="Desc">Test Description of the Item. This item contains several hazardous materials, RUN!</p>
+            </div>
+            <img class="Delete" src={Trashcan} title="Delete Item"/>
+        </div>
+    )
+}
