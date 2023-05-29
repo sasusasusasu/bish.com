a7203b31d71063b0b354114fabb017bda39e5223
JaeTriesGit <boffering@hotmail.com>
2023-05-29 12:24:44 +0300

    yes

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Checkout from './pages/checkout'
+import Login from './pages/login'
+import Register from './pages/register'
+
+export default function App(){
+    return(
+        <>
+            <Login/>
+        </>
+    )
+}
