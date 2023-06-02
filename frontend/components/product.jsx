59f32e10c0978855d042d19a277428927b65b138
jaetriesgit <boffering@hotmail.com>
2023-06-02 11:15:38 +0300

    started products page

---
+++ frontend/components/product.jsx
@@ -0,0 +1,9 @@
+export default function Product(){
+
+    return(
+        <div class="Item">
+            <img src=''/>
+            
+        </div>
+    )
+}
---
+++ frontend/pages/products.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Logo from '../assets/bish.png'
+import Product from '../components/product'
+import '../css/products.css'
+
+export default function Products(){
+    return(
+        <div id='Products-Container'>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+        </div>
+    )
+}
